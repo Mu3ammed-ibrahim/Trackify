b102c86a9df7c19211b4c6f76a79ac26498a5bb6
Muhammed Almutasim Ibrahim <www.tota.11@gmail.com>
2025-09-09 11:35:03 +0300

    adding demo account

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -186,7 +186,7 @@ const LoginPage = () => {
               <Mail className="absolute w-5 h-5 text-gray-400 transform -translate-y-1/2 left-3 top-1/2" />
               <input
                 type="email"
-                placeholder="you@example.com"
+                placeholder="demo@trackify.com"
                 className="w-full py-3 pl-10 pr-4 text-sm text-gray-900 bg-white border border-gray-300 rounded-lg dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent dark:bg-gray-700 dark:text-white md:text-base"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -204,7 +204,7 @@ const LoginPage = () => {
               <Lock className="absolute w-5 h-5 text-gray-400 transform -translate-y-1/2 left-3 top-1/2" />
               <input
                 type={showPassword ? "text" : "password"}
-                placeholder="••••••••"
+                placeholder="Demo1234"
                 className="w-full py-3 pl-10 pr-12 text-sm text-gray-900 bg-white border border-gray-300 rounded-lg dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent dark:bg-gray-700 dark:text-white md:text-base"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
